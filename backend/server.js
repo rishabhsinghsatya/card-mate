04b2295a1dc2b1a8ab4a4274feef52d0c9e9b695
Rishabh Singh <vineetsatya36@gmail.com>
2024-06-18 02:17:38 +0530

    server running and connected with mongodb

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,17 @@ import express from "express";
 import mongoose from "mongoose";
 
 const app = express();
-const PORT = 8000;
 
-const url = "";
+const PORT = process.env.PORT || 5000;
+const URL = "mongodb://127.0.0.1:27017/";
+
+// Connect to MongoDB
 mongoose
-  .connect(url, { dbName: "cardmate" })
+  .connect(URL, {
+    dbName: "card-mate",
+  })
   .then(() => {
     console.log("Connected to MongoDB");
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch((err) => console.error("Error connecting to MongoDB:", err));
-
-app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}`);
-});

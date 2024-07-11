7de87e93ce1b90c5f39f866c80b08ed2424df9ae
Rishabh Singh <vineetsatya36@gmail.com>
2024-07-11 12:35:37 +0530

    add details form to get user info-frontend

---
+++ src/components/form/FormDetails.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./FormDetails.css";
+import { useNavigate } from "react-router-dom";
+
+const FormDetails = () => {
+  const navigate = useNavigate();
+  const handleSubmit = () => {
+    navigate("/dbcards");
+  };
+  const scannnedProfiles = () => {
+    navigate("/scannedprofiles");
+  };
+  const userview = () => {
+    navigate("/userview");
+  };
+  return (
+    <div className="blueprint-container">
+      <div className="form-container">
+        <form>
+          <input type="text" placeholder="Username" />
+          <input type="email" placeholder="Email" />
+          <input type="text" placeholder="Image" />
+          <input type="text" placeholder="LinkedIn" />
+          <input type="text" placeholder="Github" />
+          <input type="text" placeholder="Contact No" />
+          <button type="submit" onClick={userview}>
+            GENERATE QR
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default FormDetails;

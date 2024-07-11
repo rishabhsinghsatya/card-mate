ee2ae10ef6b4af654d3ad8fb83092ccee1fb674e
Rishabh Singh <vineetsatya36@gmail.com>
2024-07-11 12:32:33 +0530

    add router for redirect or routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import SignUpForm from "./components/signup/Signup";
+import FormDetails from "./components/form/FormDetails";
+import DBCards from "./components/DBCards/DBCards";
+import ScannedProfiles from "./components/scanned profile/ScannedProfiles";
+import FriendList from "./components/friend list/FriendList";
+import UserView from "./components/user view/UserView";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Card-Mate</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<SignUpForm />} />
+        <Route path="/detailsform" element={<FormDetails />} />
+        <Route path="/dbcards" element={<DBCards />} />
+        <Route path="/userview" element={<UserView />} />
+        <Route path="/scannedprofiles" element={<ScannedProfiles />} />
+        <Route path="/friendslist" element={<FriendList />} />
+      </Routes>
+    </Router>
+    // <UserView />
   );
 }
 
